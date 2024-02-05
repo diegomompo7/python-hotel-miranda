217d82fa7e94f253e38f21c9606e65fb389ab940
diegomompo7 <diego.mompo.redoli@gmail.com>
2024-02-05 18:21:25 +0100

    Routes

--- django_BBDD/polls/views.py
+++ django_BBDD/polls/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+def roomId(request, question_id):
+    response = "You're looking at the results of id %s."
+    return HttpResponse(response % question_id)
